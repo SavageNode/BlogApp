0cdf917bcedea7c05123f8c1febd584e5eec59e6
Dejan Savic <savic.dejan.business@gmail.com>
2022-04-29 14:09:54 -0500

    working File

--- bloggapp/src/main/java/com/blogweb/bloggapp/BloggappApplication.java
+++ bloggapp/src/main/java/com/blogweb/bloggapp/BloggappApplication.java
@@ -2,8 +2,10 @@ package com.blogweb.bloggapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.blogweb")
 public class BloggappApplication {
 
 	public static void main(String[] args) {
--- bloggapp/src/main/java/com/blogweb/controller/ArticleController.java
+++ bloggapp/src/main/java/com/blogweb/controller/ArticleController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.blogweb.bloggapp.controller;
+package com.blogweb.controller;
 
 import com.blogweb.dao.ArticleDao;
 import com.blogweb.entities.Article;
